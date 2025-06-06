73f8b25d3e1f289b3d2f826641f445a4deac9854
SwiftAkira <175894017+SwiftAkira@users.noreply.github.com>
2025-06-06 11:18:14 +0200

    feat: Add always-on-top overlay with global hotkey

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 
-function createWindow () {
-  const win = new BrowserWindow({
+let mainWindow;
+let overlayWindow;
+
+function createMainWindow () {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -9,10 +12,41 @@ function createWindow () {
     }
   });
 
-  win.loadFile('index.html');
+  mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow);
+function createOverlayWindow() {
+  overlayWindow = new BrowserWindow({
+    width: 300,
+    height: 100,
+    frame: false,
+    alwaysOnTop: true,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  overlayWindow.loadFile('overlay.html');
+  overlayWindow.hide();
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+  createOverlayWindow();
+
+  globalShortcut.register('CommandOrControl+Shift+X', () => {
+    if (overlayWindow.isVisible()) {
+      overlayWindow.hide();
+    } else {
+      overlayWindow.show();
+    }
+  });
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -22,6 +56,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createMainWindow();
   }
 }); 
